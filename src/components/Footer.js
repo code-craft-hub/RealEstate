b974a7abede91d936503465992fa65ce1d2039f7
Kachi <125273593+KachiBlockchain@users.noreply.github.com>
2023-11-22 15:30:06 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className="bg-primary py-12">
+  return <footer className="bg-slate-200 py-12">
     <div className='container mx-auto text-white flex justify-between'>
       
       <p className="text-black">Copyright &copy; 2023. All rights reserved <b className='text-black'> KACHI </b>.</p>
