54dd92ef9bef504b447826854679cef5c8ebd7a0
Kachi <125273593+KachiBlockchain@users.noreply.github.com>
2023-11-22 15:29:18 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return <footer className="bg-slate-100 py-12">
     <div className='container mx-auto text-white flex justify-between'>
       
-      Copyright &copy; 2023. All rights reserved <b className='text-accent'> KACHI </b>.
+      <p className="text-black">Copyright &copy; 2023. All rights reserved <b className='text-black'> KACHI </b>.</p>
     </div>
   </footer>;
 };
