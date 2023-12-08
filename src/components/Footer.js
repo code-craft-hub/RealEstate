78de41a617353a2c659b00442f0dc0cd7d42842d
Kachi <125273593+KachiBlockchain@users.noreply.github.com>
2023-12-08 08:07:38 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return <footer className="bg-white border-t border-black py-12">
     <div className='container mx-auto text-white flex justify-between'>
       
-      <p className="text-black flex justify-between">Copyright &copy; 2023. All rights reserved - <a href="https://codecrafthub.tech" className='text-black'> CodeCraftHub </a>.</p>
+      <p className="text-black text-center items-center flex justify-center">Copyright &copy; 2023. All rights reserved - <a href="https://codecrafthub.tech" className='text-black'> CodeCraftHub </a>.</p>
     </div>
   </footer>;
 };
